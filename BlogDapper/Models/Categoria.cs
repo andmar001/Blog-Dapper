1493c8b602b85654668255b8ccbeefcbd6ea4494
tony tolver <andrademaresantonio00@gmail.com>
2023-03-03 20:55:15 -0600

    Repositorio CRUD categoria

---
+++ BlogDapper/Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+
+namespace BlogDapper.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int IdCategoria { get; set; }
+
+        [Required(ErrorMessage ="El nombre de categoría es obligatorio")]
+        public string Nombre { get; set; }
+        public DateTime FechaCreacion { get; set; }     
+
+    }
+}
