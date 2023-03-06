b9d8dd23ba5cb42734b4d0c5923757179da34b1c
hp_computer <andrademareshp@gmail.com>
2023-03-05 22:51:14 -0600

    Crear categoria ----> corrección de librerias frontend

--- BlogDapper/Areas/Admin/Controllers/CategoriasController.cs
+++ BlogDapper/Areas/Admin/Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
-﻿using BlogDapper.Repositorio;
+﻿using BlogDapper.Models;
+using BlogDapper.Repositorio;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogDapper.Areas.Admin.Controllers
@@ -20,6 +21,18 @@ namespace BlogDapper.Areas.Admin.Controllers
         public IActionResult Crear()
         {
             return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Crear([Bind("IdCategoria,Nombre,FechaCreacion")] Categoria categoria)
+        {
+            //validaciones del modelo
+            if(ModelState.IsValid)
+            {
+                _repoCategoria.CrearCategoria(categoria);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoria);
         }
 
         #region
--- BlogDapper/wwwroot/js/categorias.js
+++ BlogDapper/wwwroot/js/categorias.js
@@ -20,12 +20,12 @@ function cargarDatatable(){
                 "render": function (data) {
                     return `
                         <div class="text-center">
-                            <a href="/admin/categorias/editar/${data}" class="btn btn-success text-white" style="cursor:pointer; width:100px;">
-                            <i class="bi bi-pencil-square" style="margin-right:5px></i>Editar
+                            <a href="/admin/categorias/editar/${data}" class="btn btn-success text-white" style="cursor:pointer; width:100px">
+                            <i class="bi bi-pencil-square"></i>Editar
                             </a>
                             &nbsp;
-                            <a onclick=Delete("/admin/categorias/borrarCategoria/${data}") class="btn btn-danger text-white" style="cursor:pointer; width:100px;">
-                            <i class="bi bi-x-square" style="margin-right:5px></i>Borrar
+                            <a onclick=Delete("/admin/categorias/BorrarCategoria/${data}") class="btn btn-danger text-white" style="cursor:pointer; width:100px">
+                            <i class="bi bi-x-square"></i>Borrar
                             </a>
                         </div>
                     `;
