2fb04bc3082633f357f9f549c019dbffc6b23ece
tony tolver <andrademaresantonio00@gmail.com>
2023-03-25 16:18:50 -0600

    modelo articuloEtiquetas

---
+++ BlogDapper/Models/ArticuloEtiquetas.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+
+namespace BlogDapper.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int IdCategoria { get; set; }
+
+        [Required(ErrorMessage ="El nombre de categoría es obligatorio")]
+        public string Nombre { get; set; }
+        public DateTime FechaCreacion { get; set; }   
+        
+        //Esta indica la relación con articulo, donde una categoria puede tener muchosn articulos
+        public List<Articulo> Articulo { get; set; }
+
+    }
+}
