c81a76cd5b51e5f7b7868fef2329e63aad480f24
tony tolver <andrademaresantonio00@gmail.com>
2023-03-26 00:01:02 -0600

    Método de acceso Login de usuario

--- BlogDapper/Areas/Front/Controllers/AccesosController.cs
+++ BlogDapper/Areas/Front/Controllers/AccesosController.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlogDapper.Models;
+using Dapper;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol.Plugins;
 using System.Data;
 using System.Data.SqlClient;
+using System.Security.Claims;
 
 namespace BlogDapper.Areas.Front.Controllers
 {
@@ -17,5 +23,56 @@ namespace BlogDapper.Areas.Front.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UserLogin(Usuario user)
+        {
+            if (ModelState.IsValid) 
+            {
+                var sql = "SELECT * FROM Usuario WHERE Login=@Login AND Password=@Password";
+
+                //encriptar password a md5, antes de enviar la pregunta
+                var Password = obtenerMD5(user.Password);
+
+                var validar = _bd.Query(sql, new
+                {
+                    user.Login,
+                    Password
+                });
+
+                //parte de creacion de la coockie
+                if (validar.Count() == 1)
+                {
+                    //creacion de claims
+                    var claims = new List<Claim>
+                    {
+                        //guardar el login del usuario
+                        new Claim(ClaimTypes.Name, user.Login)
+                    };
+
+                    //Login - corresponde con el campo "Login" de la base de datos
+                    var claimsIdentity = new ClaimsIdentity(claims, "Login");
+
+                    //autenticar
+                    HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, 
+                        new ClaimsPrincipal(claimsIdentity));
+
+                    //llevar al inicio - dar acceso una vez autenticado
+                    return RedirectToAction("Index", "Inicio");
+                }
+                else
+                {
+                    TempData["mensajeConfirmacion"] = "Datos de acceso incorrectos";
+                    return RedirectToAction("Acceso", "Accesos");
+                }
+            }
+            else
+            {
+                TempData["mensajeConfirmacion"] = "Algunos de los campos obligatorios estan vacíos";
+                return RedirectToAction("Acceso", "Accesos");
+            }
+        }
+
     }
 }
