da9d01f2c42b3068cd125bc2610b09c104f7bce1
tony tolver <andrademaresantonio00@gmail.com>
2023-03-25 22:56:47 -0600

    Modelo de usuario

---
+++ BlogDapper/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogDapper.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public Guid User_ID { get; set; }
+        [Required(ErrorMessage ="Este campo es obligatorio")]
+        [StringLength(20, ErrorMessage = "El {0} debe ser al menos {2} y máximo {1} caracteres", MinimumLength = 3)]
+        public string Login { get; set; }
+        [Required(ErrorMessage ="Este campo es obligatorio")]
+        [StringLength(20, ErrorMessage = "El {0} debe ser al menos {2} y máximo {1} caracteres", MinimumLength = 3)]
+        public string Password { get; set; }
+    }
+}
