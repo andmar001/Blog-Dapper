554aa58f2ef82c5bdd326457d75357677b3c1baf
tony tolver <andrademaresantonio00@gmail.com>
2023-04-03 23:22:04 -0600

    Se agrego slider de manera dinamica desde la bd

---
+++ BlogDapper/Models/Slider.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+
+namespace BlogDapper.Models
+{
+    public class Slider
+    {
+        [Key]
+        public int IdSlider { get; set; }
+
+        [Required(ErrorMessage ="El nombre del slider es obligatorio")]
+        public string Nombre { get; set; }
+        public string UrlImagen { get; set; }
+
+    }
+}
